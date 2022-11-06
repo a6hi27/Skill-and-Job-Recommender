dd6015e1290af57c60c996e71e2582eff3471f11
a6hi27 <2k19cse049@kiot.ac.in>
2022-11-06 23:12:56 +0530

    Update on Sprint 2

--- Project Development Phase/Sprint 2/app.py
+++ Project Development Phase/Sprint 2/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import ibm_db
 from flask_mail import Mail, Message
 from random import randint
+import json
 import os
 import csv
 import pathlib
@@ -19,25 +20,14 @@ connection = ibm_db.connect(connectionstring, '', '')
 app = Flask(__name__)
 app.debug = True
 
-arr = []
-role='Senior Software Developer'
-with open("Company_Database.csv", 'r') as file:
-    csvreader = csv.reader(file)
-    for i in csvreader:
-        if i[2] == role:
-            dict={
-                'cname':i[1],'role':i[2],'ex':i[3],'skill':i[4],'vacancy':i[5],'stream':i[6],'job_location':i[7],'salary':i[8]
-            }
-            arr.append(dict)
 
 
 mail = Mail(app)
 app.secret_key = "HireMe.com"
-useremail = ""
+
 first_name = ""
 last_name = ""
 password = ""
-newuser = None
 
 
 app.config["MAIL_SERVER"] = 'smtp.gmail.com'
@@ -74,20 +64,16 @@ def signup():
 
 @app.route('/verification', methods=["POST", "GET"])
 def verify():
+    global first_name
+    global last_name
+    global password
+    global otp
 
     if request.method == 'POST':
-        global first_name
-        global last_name
-        global useremail
-        global password
-        global email
-        global otp
-
         first_name = request.form.get('first_name')
         last_name = request.form.get('last_name')
-        useremail = request.form.get('email')
         password = request.form.get('password')
-
+        useremail = request.form.get('email')
         sql = "SELECT * FROM User WHERE email =?"
         stmt = ibm_db.prepare(connection, sql)
         ibm_db.bind_param(stmt, 1, useremail)
@@ -96,46 +82,49 @@ def verify():
 
         if (account):
             return render_template('signup.html', msg="You are already a member, please login using your details")
-
         else:
+            session['regmail'] = useremail
             otp = randint(000000, 999999)
-            email = request.form['email']
             msg = Message(subject='OTP', sender='hackjacks@gmail.com',
-                          recipients=[email])
+                          recipients=[session['regmail']])
             msg.body = "You have succesfully registered for Hire Me!\nUse the OTP given below to verify your email ID.\n\t\t" + \
                 str(otp)
             mail.send(msg)
             return render_template('verification.html')
 
-    if request.method == 'GET':
-        otp = randint(000000, 999999)
-        msg = Message(subject='OTP', sender='hackjacks@gmail.com',
-                      recipients=[email])
-        msg.body = "You have succesfully registered for Hire Me!\nUse the OTP given below to verify your email ID.\n\t\t" + \
-            str(otp)
-        mail.send(msg)
-        return render_template('verification.html', resendmsg="OTP has been resent")
-
-
-@app.route('/validate', methods=['POST'])
-def validate():
-    global first_name
-    global last_name
-    global password
-    global useremail
-    user_otp = request.form['otp']
-    if otp == int(user_otp):
-        insert_sql = "INSERT INTO User(first_name,last_name,email,pass) VALUES (?,?,?,?)"
-        prep_stmt = ibm_db.prepare(connection, insert_sql)
-        ibm_db.bind_param(prep_stmt, 1, first_name)
-        ibm_db.bind_param(prep_stmt, 2, last_name)
-        ibm_db.bind_param(prep_stmt, 3, useremail)
-        ibm_db.bind_param(prep_stmt, 4, password)
-        ibm_db.execute(prep_stmt)
-        return render_template('signin.html')
-
+    elif ("regmail" in session):
+        if request.method == 'GET':
+            otp = randint(000000, 999999)
+            msg = Message(subject='OTP', sender='hackjacks@gmail.com',
+                          recipients=[session['regmail']])
+            msg.body = "You have succesfully registered for Hire Me!\nUse the OTP given below to verify your email ID.\n\t\t" + \
+                str(otp)
+            mail.send(msg)
+            return render_template('verification.html', resendmsg="OTP has been resent")
     else:
-        return render_template('verification.html', msg="OTP is invalid. Please enter a valid OTP")
+        return redirect('/')
+
+
+@app.route('/validate', methods=['POST', 'GET'])
+def validate():
+    if ('regmail' in session):
+        global first_name
+        global last_name
+        global password
+        user_otp = request.form['otp']
+        if otp == int(user_otp):
+            insert_sql = "INSERT INTO User(first_name,last_name,email,pass) VALUES (?,?,?,?)"
+            prep_stmt = ibm_db.prepare(connection, insert_sql)
+            ibm_db.bind_param(prep_stmt, 1, first_name)
+            ibm_db.bind_param(prep_stmt, 2, last_name)
+            ibm_db.bind_param(prep_stmt, 3, session['regmail'])
+            ibm_db.bind_param(prep_stmt, 4, password)
+            ibm_db.execute(prep_stmt)
+            return render_template('signin.html')
+        else:
+            return render_template('verification.html', msg="OTP is invalid. Please enter a valid OTP")
+    else:
+        return redirect('/')
 
 
 @app.route("/googlelogin")
@@ -228,21 +217,33 @@ def twitter_callback():
 
 @app.route("/logout")
 def logout():
-    session.clear()
-    return redirect("/signin")
+    session.pop('useremail', None)
+    session.pop('regmail', None)
+    session.pop('newuser', None)
+    return redirect("/login")
 
 
 @app.route("/home")
 def home():
-    return render_template("index.html",arr=arr)
+    if "useremail" in session:
+        arr = []
+        role = session['role']
+        with open("Company_Database.csv", 'r') as file:
+            csvreader = csv.reader(file)
+            for i in csvreader:
+                if i[2] == role:
+                    dict = {
+                        'cname': i[1], 'role': i[2], 'ex': i[3], 'skill': i[4], 'vacancy': i[5], 'stream': i[6], 'job_location': i[7], 'salary': i[8]
+                    }
+                    arr.append(dict)
+        companies = json.dumps(arr)
+        return render_template("index.html", companies=companies, arr=arr)
+    else:
+        return redirect('/login')
 
 
-@app.route("/signin")
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    global useremail
-    global newuser
-    # Login is the first page which the user encounters
     if request.method == 'POST':
         useremail = request.form.get('email')
         password = request.form.get('password')
@@ -252,86 +253,146 @@ def login():
         ibm_db.execute(stmt)
         account = ibm_db.fetch_assoc(stmt)
         if account:
-            newuser = account['NEWUSER']
+            session["useremail"] = useremail
+            session["newuser"] = account['NEWUSER']
             if (password == str(account['PASS']).strip()):
-                if (newuser == 1):
+                if (session['newuser'] == 1):
                     return redirect('/profile')
-                return redirect('/home')
+                else:
+                    sql = "SELECT * FROM profile WHERE email_id =?"
+                    stmt = ibm_db.prepare(connection, sql)
+                    ibm_db.bind_param(stmt, 1, useremail)
+                    print(useremail)
+                    ibm_db.execute(stmt)
+                    account = ibm_db.fetch_assoc(stmt)
+                    session['role']=account['JOB_TITLE']
+                    print(session['role'])
+                    return redirect('/home')
             else:
                 return render_template('signin.html', msg="Password is invalid")
         else:
             return render_template('signin.html', msg="Email is invalid")
     else:
-        return render_template('signin.html')
+        if "useremail" in session:
+            return redirect('/home')
+        else:
+            return render_template('signin.html')
 
 
 @app.route("/profile", methods=["POST", "GET"])
 def profile():
-    global newuser
-    global useremail
-    select_sql = "SELECT * FROM user where email= ?"
-    prep_stmt = ibm_db.prepare(connection, select_sql)
-    ibm_db.bind_param(prep_stmt, 1, useremail)
-    ibm_db.execute(prep_stmt)
-    account = ibm_db.fetch_assoc(prep_stmt)
-    if (newuser == 1 and request.method == 'POST'):
+    if "useremail" in session:
+        if (session['newuser'] == 1 and request.method == 'POST'):
+            first_name = request.form.get('first_name')
+            last_name = request.form.get('last_name')
+            mobile_no = request.form.get('mobile_no')
+            address_line_1 = request.form.get('address_line_1')
+            address_line_2 = request.form.get('address_line_2')
+            zipcode = request.form.get('zipcode')
+            city = request.form.get('city')
+            education = request.form.get('education')
+            country = request.form.get('countries')
+            state = request.form.get('states')
+            experience = request.form.get('experience')
+            job_title = request.form.get('job_title')
 
-        first_name = request.form.get('first_name')
-        last_name = request.form.get('last_name')
-        mobile_no = request.form.get('mobile_no')
-        address_line_1 = request.form.get('address_line_1')
-        address_line_2 = request.form.get('address_line_2')
-        zipcode = request.form.get('zipcode')
-        city = request.form.get('city')
-        education = request.form.get('education')
-        country = request.form.get('countries')
-        state = request.form.get('states')
-        experience = request.form.get('experience')
-        job_title = request.form.get('job_title')
+            insert_sql = "INSERT INTO profile VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)"
 
-        insert_sql = "INSERT INTO profile VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)"
+            prep_stmt = ibm_db.prepare(connection, insert_sql)
+            ibm_db.bind_param(prep_stmt, 1, first_name)
+            ibm_db.bind_param(prep_stmt, 2, last_name)
+            ibm_db.bind_param(prep_stmt, 3, mobile_no)
+            ibm_db.bind_param(prep_stmt, 4, address_line_1)
+            ibm_db.bind_param(prep_stmt, 5, address_line_2)
+            ibm_db.bind_param(prep_stmt, 6, zipcode)
+            ibm_db.bind_param(prep_stmt, 7, city)
+            ibm_db.bind_param(prep_stmt, 8, session['useremail'])
+            ibm_db.bind_param(prep_stmt, 9, education)
+            ibm_db.bind_param(prep_stmt, 10, country)
+            ibm_db.bind_param(prep_stmt, 11, state)
+            ibm_db.bind_param(prep_stmt, 12, experience)
+            ibm_db.bind_param(prep_stmt, 13, job_title)
+            ibm_db.execute(prep_stmt)
 
-        prep_stmt = ibm_db.prepare(connection, insert_sql)
-        ibm_db.bind_param(prep_stmt, 1, first_name)
-        ibm_db.bind_param(prep_stmt, 2, last_name)
-        ibm_db.bind_param(prep_stmt, 3, mobile_no)
-        ibm_db.bind_param(prep_stmt, 4, address_line_1)
-        ibm_db.bind_param(prep_stmt, 5, address_line_2)
-        ibm_db.bind_param(prep_stmt, 6, zipcode)
-        ibm_db.bind_param(prep_stmt, 7, city)
-        ibm_db.bind_param(prep_stmt, 8, useremail)
-        ibm_db.bind_param(prep_stmt, 9, education)
-        ibm_db.bind_param(prep_stmt, 10, country)
-        ibm_db.bind_param(prep_stmt, 11, state)
-        ibm_db.bind_param(prep_stmt, 12, experience)
-        ibm_db.bind_param(prep_stmt, 13, job_title)
-        ibm_db.execute(prep_stmt)
+            insert_sql = "UPDATE USER SET newuser = false WHERE email=?"
+            session['newuser'] = 0
+            prep_stmt = ibm_db.prepare(connection, insert_sql)
+            ibm_db.bind_param(prep_stmt, 1, session['useremail'])
+            ibm_db.execute(prep_stmt)
+            return redirect('/home')
 
-        insert_sql = "UPDATE USER SET newuser = false WHERE email=?"
-        prep_stmt = ibm_db.prepare(connection, insert_sql)
-        ibm_db.bind_param(prep_stmt, 1, useremail)
-        ibm_db.execute(prep_stmt)
-        return render_template('index.html', newuser=newuser)
+        if (session['newuser'] == 0):
+            sql = "SELECT * FROM profile WHERE email_id =?"
+            stmt = ibm_db.prepare(connection, sql)
+            ibm_db.bind_param(stmt, 1, session['useremail'])
+            ibm_db.execute(stmt)
+            account = ibm_db.fetch_assoc(stmt)
+            first_name = account['FIRST_NAME']
+            last_name = account['LAST_NAME']
+            mobile_no = account['MOBILE_NUMBER']
+            address_line_1 = account['ADDRESS_LINE_1']
+            address_line_2 = account['ADDRESS_LINE_2']
+            zipcode = account['ZIPCODE']
+            education = account['EDUCATION']
+            countries = account['COUNTRY']
+            states = account['STATEE']
+            city = account['CITY']
+            experience = account['EXPERIENCE']
+            job_title = account['JOB_TITLE']
+            return render_template('profile.html', email=session['useremail'], newuser=session['newuser'], first_name=first_name, last_name=last_name, address_line_1=address_line_1, address_line_2=address_line_2, zipcode=zipcode, education=education, countries=countries, states=states, experience=experience, job_title=job_title, mobile_no=mobile_no, city=city)
 
-    if (newuser == 0):
-        sql = "SELECT * FROM profile WHERE email_id =?"
+        else:
+            return render_template('profile.html', newuser=session['newuser'], email=session['useremail'])
+    else:
+        return redirect("/login")
+
+
+@app.route("/forgotpass", methods=["POST", "GET"])
+def forgotpass():
+    global i
+    global otp
+    global email
+
+    if request.method == 'POST':
+
+        useremail = request.form.get('email')
+        user_otp = request.form.get('OTP')
+        password = request.form.get('password')
+
+        sql = "SELECT * FROM User WHERE email =?"
         stmt = ibm_db.prepare(connection, sql)
         ibm_db.bind_param(stmt, 1, useremail)
         ibm_db.execute(stmt)
         account = ibm_db.fetch_assoc(stmt)
-        first_name = account['FIRST_NAME']
-        last_name = account['LAST_NAME']
-        mobile_no = account['MOBILE_NUMBER']
-        address_line_1 = account['ADDRESS_LINE_1']
-        address_line_2 = account['ADDRESS_LINE_2']
-        zipcode = account['ZIPCODE']
-        education = account['EDUCATION']
-        countries = account['COUNTRY']
-        states = account['STATEE']
-        city = account['CITY']
-        experience = account['EXPERIENCE']
-        job_title = account['JOB_TITLE']
-        return render_template('profile.html', email=useremail, newuser=newuser, first_name=first_name, last_name=last_name, address_line_1=address_line_1, address_line_2=address_line_2, zipcode=zipcode, education=education, countries=countries, states=states, experience=experience, job_title=job_title, mobile_no=mobile_no, city=city)
 
-    else:
-        return render_template('profile.html', newuser=newuser, email=useremail)
+        if i == 1:
+            if otp == int(user_otp):
+                i = 2
+                return render_template('forgotpass.html', i=i)
+            else:
+                return render_template('forgotpass.html', msg="OTP is invalid. Please enter a valid OTP", i=i)
+
+        elif i == 2:
+            sql = "UPDATE USER SET pass=? WHERE email=?"
+            stmt = ibm_db.prepare(connection, sql)
+            ibm_db.bind_param(stmt, 1, password)
+            ibm_db.bind_param(stmt, 2, email)
+            ibm_db.execute(stmt)
+            i = 1
+            return render_template('signin.html')
+
+        elif i == 0:
+            if (account):
+                otp = randint(000000, 999999)
+                email = request.form['email']
+                msg = Message(subject='OTP', sender='hackjacks@gmail.com',
+                              recipients=[email])
+                msg.body = "Forgot your password?\n\nWe received a request to reset the password for your account.Use the OTP given below to reset the password.\n\n" + \
+                    str(otp)
+                mail.send(msg)
+                i = 1
+                return render_template('forgotpass.html', i=i)
+            else:
+                return render_template('forgotpass.html', msg="It looks like you are not yet our member!")
+    i = 0
+    return render_template('forgotpass.html')
